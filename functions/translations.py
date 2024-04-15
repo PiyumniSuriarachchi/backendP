6d492cca28c18d4de7d06aaa71d53da04f0fe5cd
PiyumniSuriarachchi <163654071+PiyumniSuriarachchi@users.noreply.github.com>
2024-04-15 17:35:52 +0530

    Update translations.py

--- functions/translations.py
+++ functions/translations.py
@@ -2,9 +2,28 @@
 from deep_translator import GoogleTranslator
 
 def split_text(text, chunk_size=500):
+    """
+    Split a given text into chunks of specified size.
+    
+    Args:
+    text (str): The text to be split into chunks.
+    chunk_size (int): The size of each chunk.
+    
+    Returns:
+    list: A list of text chunks.
+    """
     return [text[i:i + chunk_size] for i in range(0, len(text), chunk_size)]
 
 def translate_text(data):
+    """
+    Translate text to the target language using Google Translator.
+    
+    Args:
+    data (dict): A dictionary containing the text to be translated and the target language.
+    
+    Returns:
+    dict: A dictionary containing the translated text.
+    """
     try:
         text_to_translate = data.get('text', '')
         target_language = data.get('target_language', 'en')
